7977d20593929ad9599e04bde8751f07bbd6bbc5
Christoph Kretschmer <christoph@kretschmer-und-kretschmer.de>
2022-11-22 22:56:15 +0100

    proper distance

---
+++ ros/robot_ws/src/robot/robot/ultrasonic.py
@@ -0,0 +1,65 @@
+import time
+import RPi.GPIO as GPIO
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Number
+
+class Ultrasonic(Node):
+  def __init__(self):
+    super().__init__('ultrasonic')
+    self.publisher_ = self.create_publisher(String, 'topic', 10)
+    timer_period = 0.5  # seconds
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+    
+    GPIO.setwarnings(False)
+    self.trigger_pin = 27
+    self.echo_pin = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.trigger_pin,GPIO.OUT)
+    GPIO.setup(self.echo_pin,GPIO.IN)
+    
+  def timer_callback(self):
+    msg = Number()
+    msg.data = self.get_distance()
+    self.publisher_.publish(msg)
+
+  def send_trigger_pulse(self):
+    GPIO.output(self.trigger_pin,True)
+    time.sleep(0.00015)
+    GPIO.output(self.trigger_pin,False)
+
+  def wait_for_echo(self,value,timeout):
+    count = timeout
+    while GPIO.input(self.echo_pin) != value and count>0:
+      count = count-1
+    
+  def get_distance(self):
+    distance_cm=[0,0,0,0,0]
+    for i in range(3):
+      self.send_trigger_pulse()
+      self.wait_for_echo(True,10000)
+      start = time.time()
+      self.wait_for_echo(False,10000)
+      finish = time.time()
+      pulse_len = finish-start
+      distance_cm[i] = pulse_len/0.000058
+    distance_cm=sorted(distance_cm)
+    return int(distance_cm[2])
+  
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  ultrasonic = Ultrasonic()
+
+  rclpy.spin(ultrasonic)
+
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  ultrasonic.destroy_node()
+  rclpy.shutdown()
+  
+if __name__ == '__main__':
+  main()
--- ros/robot_ws/src/robot/setup.py
+++ ros/robot_ws/src/robot/setup.py
@@ -21,6 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ultrasonic = robot.ultrasonic:main',
         ],
     },
 )
